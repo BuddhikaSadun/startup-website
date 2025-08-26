dbde509a04f1c3d70edfb4fee0d3bc87b72348dc
BuddhikaSadun <sadunjayamaha917@gmail.com>
2025-08-26 23:29:18 +0530

    Initial react vite files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/layout/Header";
+import Hero from "./components/other/Hero";
+import About from "./components/other/About";
+import Services from "./components/other/Services";
+import Portfolio from "./components/other/Portfolio";
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Services />
+      <Portfolio />
+    </>
+  );
+}
+
+export default App;
